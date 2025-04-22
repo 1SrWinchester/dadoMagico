b34f11333efa4ad06a57492e89833bdf787fc690
Jose Angel <joseangelsnchz8@gmail.com>
2025-04-22 21:52:01 +0200

    Mejorado

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-//corrección del error a la hora de agrandar y disminuir tamaño
+// corrección del error a la hora de agrandar y disminuir tamaño
 document.addEventListener("DOMContentLoaded", () => {
-    const box = document.querySelector(".box");
-  
-    box.addEventListener("click", () => {
-      const numero = Math.floor(Math.random() * 6) + 1;
-      box.textContent = numero;
-      box.classList.add("on");
-      
-      // Cambiar tamaño basado en el número
-      const nuevoTamaño = 100 + numero * 10; // Aumenta el tamaño con el número
-      box.style.width = `${nuevoTamaño}px`;
-      box.style.height = `${nuevoTamaño}px`;
-      setTimeout(() => {
-        box.classList.remove("on");
-        box.style.width = "100px"; // Resetear tamaño
-        box.style.height = "100px"; // Resetear tamaño
-      }, 2000);
-      
-      // Mostrar mensaje personalizado
-      if (numero === 6) {
-      alert("¡Felicidades! Has sacado el número más alto.");
-    } else if (numero === 1) {
-      alert("¡Sigue intentándolo! Podrías sacar un 6.");
+  const box = document.querySelector(".box");
+  const mensaje = document.querySelector(".mensaje"); // Capturamos el h1
+
+  box.addEventListener("click", () => {
+    const numero = Math.floor(Math.random() * 6) + 1;
+    box.textContent = numero;
+    box.classList.add("on");
+    // Cambiar tamaño basado en el número
+    const nuevoTamaño = 100 + numero * 10; // Aumenta el tamaño con el número
+    box.style.width = `${nuevoTamaño}px`;
+    box.style.height = `${nuevoTamaño}px`;
+    setTimeout(() => {
+      box.classList.remove("on");
+      box.style.width = "100px"; // Resetear tamaño
+      box.style.height = "100px"; // Resetear tamaño
+    }, 2000);
+    if (numero === 6) {
+      mensaje.textContent = "¡Felicidades! Has sacado el número más alto";
+      mensaje.style.color = "gold";
+    } else if (numero <= 5) {
+      mensaje.textContent = "¡Sigue intentándolo!";
+      mensaje.style.color = "red";
     }
-    });
   });
+});
